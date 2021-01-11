cf5954a1056188bbca6c1800a0ddc8f626b61de1
jon.snow <jon.snow@kakaocorp.com>
2021-01-11 14:16:34 +0900

    refactor(LottoNumber): thread-safe하게 리팩터

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -6,9 +6,13 @@ public class LottoNumber implements Comparable<LottoNumber> {
     private final int number;
     private static final LottoNumber[] LIST = new LottoNumber[46];
 
+    static {
+        for(int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++){
+            LIST[i] = new LottoNumber(i);
+        }
+    }
+
     public static LottoNumber getInstance(int number) {
-        if (LIST[number] != null) return LIST[number];
-        LIST[number] = new LottoNumber(number);
         return LIST[number];
     }
 
