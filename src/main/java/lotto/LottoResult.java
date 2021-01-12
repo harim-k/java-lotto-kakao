3cc3bd26fcf49672a753fe352f88cda1326cd676
jon.snow <jon.snow@kakaocorp.com>
2021-01-12 12:30:23 +0900

    refactor: 긴 숫자에 '_' 추가, 상수 제거

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -3,27 +3,21 @@ package lotto;
 import java.util.Arrays;
 
 public enum LottoResult {
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
+    FIRST(6, false, 2000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1500_000),
     FOURTH(4, false, 50000),
     FIFTH(3, false, 5000),
     NOTHING(0, false, 0);
-
-    public static final int FIRST_WINNER_MATCH_CONT = 6;
-    public static final int SECOND_WINNER_MATCH_COUNT = 5;
-    public static final int THIRD_WINNER_MATCH_COUNT = 5;
-    public static final int FIFTH_WINNER_MATCH_COUNT = 3;
-    public static final int FOURTH_WINNER_MATCH_COUNT = 4;
 
     private final int matchCount;
     private final boolean bonusMatch;
     private final int reward;
 
     public static LottoResult getResult(int matchCount, boolean bonusMatch) {
-        boolean finalBonusMatch = matchCount == 5 ? bonusMatch : false;
+        boolean finalBonusMatch = matchCount == SECOND.matchCount ? bonusMatch : false;
 
-        return Arrays.stream(LottoResult.values())
+        return Arrays.stream(values())
                 .filter(result -> result.matchCount == matchCount && result.bonusMatch == finalBonusMatch)
                 .findFirst()
                 .orElse(NOTHING);
@@ -41,7 +35,7 @@ public enum LottoResult {
 
     @Override
     public String toString() {
-        if (matchCount == SECOND_WINNER_MATCH_COUNT && bonusMatch)
+        if (matchCount == SECOND.matchCount && bonusMatch)
             return String.format("%d개 일치, 보너스 볼 일치(%d원)", matchCount, reward);
         return String.format("%d개 일치 (%d원)", matchCount, reward);
     }
